3af5b5720558cfd6e082eebfda0db5a00e2f795e
Carlos Rojas <caranroja@outlook.com>
2021-10-27 15:10:23 -0500

    Se agrega cors para permitir acceso de la url del front

--- loggerMiddleware.js
+++ loggerMiddleware.js
@@ -1,9 +1,9 @@
-const logger = (request, response, next)=>{
-    console.log(request.method)
-    console.log(request.path)
-    console.log(request.body)
-    console.log('-----')
-    next()
+const logger = (request, response, next) => {
+  console.log(request.method)
+  console.log(request.path)
+  console.log(request.body)
+  console.log('-----')
+  next()
 }
 
 module.exports = logger
